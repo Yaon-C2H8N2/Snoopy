8ee6f6bf61b7740835cdf72153e67e6c2cb13159
Yoan Dusoleil <y.dusoleil@outlook.com>
2023-12-20 13:14:12 +0100

    bugfix

--- api/src/main/java/fr/yaon/api/web/services/PrestationEndpoint.java
+++ api/src/main/java/fr/yaon/api/web/services/PrestationEndpoint.java
@@ -3,8 +3,10 @@ package fr.yaon.api.web.services;
 import com.fasterxml.jackson.core.JsonFactoryBuilder;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.util.JSONPObject;
+import fr.yaon.api.repositories.PrestationInterventionRepository;
 import fr.yaon.api.web.models.PrestationIntervention;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,12 +16,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/prestation")
 public class PrestationEndpoint {
+    @Autowired
+    private PrestationInterventionRepository prestationInterventionRepository;
+
     public PrestationEndpoint() {
     }
 
     @PutMapping("/save")
     public String savePrestation(@RequestBody PrestationIntervention prestationIntervention, HttpServletResponse response) {
         try {
+            prestationInterventionRepository.save(prestationIntervention);
             return new ObjectMapper().writeValueAsString(prestationIntervention);
         } catch (Exception e) {
             response.setContentType(MediaType.TEXT_PLAIN_VALUE);
