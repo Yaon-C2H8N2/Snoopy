49487db3a57914cdf9b31d921bbb0444d079c1d3
Yoan Dusoleil <y.dusoleil@outlook.com>
2024-01-16 22:38:17 +0100

    set up vite dev env

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
+
+//config for vite with proxy in dev mode only
 export default defineConfig({
-  plugins: [react()],
+    plugins: [react()],
+    server: {
+        proxy: {
+            '/api': {
+                target: 'http://localhost:8080',
+                changeOrigin: true,
+                secure: false,
+                rewrite: (path) => path.replace(/^\/api/, ''),
+            },
+        },
+    },
 })
