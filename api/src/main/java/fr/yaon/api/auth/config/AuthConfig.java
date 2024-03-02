c256a4ab6e5bcfefb32cdda3a563533475f46469
Yoan Dusoleil <y.dusoleil@outlook.com>
2024-03-02 12:05:26 +0100

    Changed password encoder to bcrypt

--- api/src/main/java/fr/yaon/api/auth/config/AuthConfig.java
+++ api/src/main/java/fr/yaon/api/auth/config/AuthConfig.java
@@ -12,7 +12,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -31,8 +31,7 @@ public class AuthConfig {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-        //TODO : change password encoder
-        return NoOpPasswordEncoder.getInstance();
+        return new BCryptPasswordEncoder();
     }
 
     @Bean
