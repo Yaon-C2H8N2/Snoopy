14f1bdbc7deb42d5cb0cf1523a8d4ce57d61107c
Yoan Dusoleil <y.dusoleil@outlook.com>
2024-03-14 22:20:53 +0100

    Ajout endpoint liste utilisateurs #6

--- api/src/main/java/fr/yaon/api/auth/repositories/UtilisateurRepository.java
+++ api/src/main/java/fr/yaon/api/auth/repositories/UtilisateurRepository.java
@@ -3,8 +3,12 @@ package fr.yaon.api.auth.repositories;
 import fr.yaon.api.auth.models.Utilisateur;
 import org.springframework.data.repository.Repository;
 
+import java.util.List;
+
 public interface UtilisateurRepository extends Repository<Utilisateur, Integer> {
     Utilisateur findByUsername(String username);
+
+    List<Utilisateur> findAll();
 
     Utilisateur save(Utilisateur utilisateur);
 }
--- api/src/main/java/fr/yaon/api/web/services/AdminEndpoint.java
+++ api/src/main/java/fr/yaon/api/web/services/AdminEndpoint.java
@@ -1,21 +1,36 @@
 package fr.yaon.api.web.services;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import fr.yaon.api.auth.models.Utilisateur;
+import fr.yaon.api.auth.repositories.UtilisateurRepository;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/admin")
 public class AdminEndpoint {
 
+    @Autowired
+    private UtilisateurRepository utilisateurRepository;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
     public AdminEndpoint() {
     }
 
-    @GetMapping("test")
-    public String getAllPrestations(HttpServletResponse response) {
+    @GetMapping("/utilisateurs")
+    public String getAllUtilisateurs(HttpServletResponse response) {
         try {
-            response.setContentType(MediaType.TEXT_PLAIN_VALUE);
-            return "Hello World";
+            List<Utilisateur> utilisateurs = this.utilisateurRepository.findAll();
+            for (Utilisateur utilisateur : utilisateurs) {
+                utilisateur.setPassword(null);
+            }
+            return this.objectMapper.writeValueAsString(utilisateurs);
         } catch (Exception e) {
             response.setContentType(MediaType.TEXT_PLAIN_VALUE);
             response.setStatus(500);
