460b65fc8833098bac2e551fe98b65a096b4ede6
Yoan Dusoleil <y.dusoleil@outlook.com>
2024-03-15 07:53:00 +0100

    Ajout et suppression de type prestation, prestation et client
    Issue: #8, #9, #10

--- api/src/main/java/fr/yaon/api/web/services/AdminEndpoint.java
+++ api/src/main/java/fr/yaon/api/web/services/AdminEndpoint.java
@@ -3,6 +3,12 @@ package fr.yaon.api.web.services;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import fr.yaon.api.auth.models.Utilisateur;
 import fr.yaon.api.auth.repositories.UtilisateurRepository;
+import fr.yaon.api.repositories.ClientRepository;
+import fr.yaon.api.repositories.PrestationRepository;
+import fr.yaon.api.repositories.TypePrestationRepository;
+import fr.yaon.api.web.models.Client;
+import fr.yaon.api.web.models.Prestation;
+import fr.yaon.api.web.models.TypePrestation;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
@@ -17,6 +23,15 @@ public class AdminEndpoint {
 
     @Autowired
     private UtilisateurRepository utilisateurRepository;
+
+    @Autowired
+    private TypePrestationRepository typePrestationRepository;
+
+    @Autowired
+    private PrestationRepository prestationRepository;
+
+    @Autowired
+    private ClientRepository clientRepository;
 
     @Autowired
     private ObjectMapper objectMapper;
@@ -66,6 +81,42 @@ public class AdminEndpoint {
                 }
                 return objectMapper.writeValueAsString(this.utilisateurRepository.deleteUtilisateurByIdUtilisateur(idUtilisateur));
             } else throw new Exception("Utilisateur not found");
+        } catch (Exception e) {
+            response.setContentType(MediaType.TEXT_PLAIN_VALUE);
+            response.setStatus(500);
+            return e.getMessage();
+        }
+    }
+
+    @PutMapping("/typeprestation")
+    public String addTypePrestation(HttpServletResponse response, @RequestBody TypePrestation typePrestation) {
+        try {
+            typePrestationRepository.save(typePrestation);
+            return objectMapper.writeValueAsString(typePrestation);
+        } catch (Exception e) {
+            response.setContentType(MediaType.TEXT_PLAIN_VALUE);
+            response.setStatus(500);
+            return e.getMessage();
+        }
+    }
+
+    @PutMapping("/prestation")
+    public String addTypePrestation(HttpServletResponse response, @RequestBody Prestation prestation) {
+        try {
+            prestationRepository.save(prestation);
+            return objectMapper.writeValueAsString(prestation);
+        } catch (Exception e) {
+            response.setContentType(MediaType.TEXT_PLAIN_VALUE);
+            response.setStatus(500);
+            return e.getMessage();
+        }
+    }
+
+    @PutMapping("/client")
+    public String addTypePrestation(HttpServletResponse response, @RequestBody Client client) {
+        try {
+            clientRepository.save(client);
+            return objectMapper.writeValueAsString(client);
         } catch (Exception e) {
             response.setContentType(MediaType.TEXT_PLAIN_VALUE);
             response.setStatus(500);
